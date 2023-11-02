443107d886e7c515a9255720b4f55d48f025c8dc
focuzzed <1limbo.theorem1@gmail.com>
2023-11-02 23:42:45 +0300

    refactor: add function for transform errors list (WIP)

--- src/shared/libs/rest/types/index.ts
+++ src/shared/libs/rest/types/index.ts
@@ -2,3 +2,4 @@ export * from './http-methods.enum.js';
 export * from './request-body.type.js';
 export * from './request-params.type.js';
 export * from './route.interface.js';
+export * from './validation-error-field.type.js';
